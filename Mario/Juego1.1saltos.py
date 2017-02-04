cadafe973b899578926a81c8514eff8117d821ae
ValeriaOchoa <dvale14toledo@gmail.com>
2017-02-03 23:11:02 -0500

    no ha sido posible implementar el mapa errores ("KeyError: 'data' ")

--- Mario/Juego1.1saltos.py
+++ Mario/Juego1.1saltos.py
@@ -1,20 +1,30 @@
 import sys, pygame
+import StringIO
+import json
+import gzip
+import base64
 from pygame.locals import *
-from Tkconstants import FALSE
- 
+from pygame.constants import K_x
+
 # Constantes
-WIDTH = 900
-HEIGHT = 500
-PposX=300#posicion de mario
-PposY=318#
+WIDTH = 900#ancho pantalla
+HEIGHT = 500#altura de pantalla
+PposX=300#posicion den x de mario
+PposY=318#posicion en y de mario 
 cont=6
 dire=True#direccion 
 i=0#guardara la posicion del arreglo 
 xixf={}#xinicio y xfin movimiento derecha
 Rxixf={}#xinicio y Xfin movimiento izquierda 
-Scurva={}
+Scurva={}#arreglo para los saltos
 saltar = False 
 saltar_Cur=False
+tWidth=0# tamaño del ancho del tile 
+tHeigth=0# tamaño de la altura del tile
+WidthMap=0# ancho del mapa
+HeightMap=0# altura del mapa
+MatrizMapa=[]# arreglo para el mapa
+
 
 #=================>>>>IMAGEN<<<<====================================
 def imagen(n_archivo, transparencia=False):# parametros el nombre de la imagen y su tipo.
@@ -97,19 +107,71 @@ def spriteMARIO():
        cont=0
    
     return
+#==================>>>>>CARGAR_MAPA<<<<<<=======================
+def cargar_mapa(Nivel1):# creamos la funcion cargar mapa y le agregamos la variable nivel1 como parametro 
+    global tWidth,tHeight,WidthMap,HeightMap,MatrizMapa# llamamos a las variables globales
+    a=open ("mapas/"+Nivel1+".json","r")#acceder al archivo
+    data=json.load(a)#almacenar los datos de la variable a
+    a.close()
+    tWidth=data["tilewidth"]
+    tHeight=data["tileheight"]
+    WidthMap=data["width"]
+    HeightMap=data["height"]
+    #tomar el mapa
+    for item in data["layers"]:
+        mapa=item["data"]
+    print mapa
+    mapa=base64.decoestring(mapa)#traducir
+    print mapa
+    cadena=gzip.zlib.decompress(mapa);#descomprimir los bloques de la imagen
+    print cadena
+    salidaMapa=[]
+    for idx in xrange(0,len(cadena),4):#algoritmo de conversion a numeros de los caracteres
+        val=odr(str(cadena[idx]))|(odr(str(cadena[idx+1]))<<8)|\
+        (odr(str(cadena[idx+2]))<<16)| (odr(str(cadena[idx+3]))<<24)
+        salidaMapa.append(val)
+    print salidaMapa
+    for i in range(0, len(salidaMapa),WidthMap):# convertir en matriz el mapa, for desde 0 al tamaño del mapa con porsiones del ancho del mapa.
+        MatrizMapa.append(salidaMapa[i:i+WidthMap])# (tramos) añadir datos al final del mapa con la posision deseada y su ancho
+    for i in range(HeightMap):
+        print MatrizMapa
+def arreglo_Tiles(img):#obritne imagen y lo almacena en hoja de tiles de tamaño 29 de alto y 27 de ancho
+    x=0
+    y=0
+    hojaTiles=[]
+    for i in range(29):
+        for j in range(27):
+            imagen=cortar(img,(x,y,16,16))
+            hojaTiles.append(imagen)
+            x+=18
+        x=0
+        y+=18
+    return hojaTiles
+    
+def cortar(img,rectan):
+    rec=pygame.Rect(rectan)
+    image=pygame.Surface(rec.size).convert()
+    image.blit(img,(0,0),rect)
+    return image 
+
+    
+
 #==================>>>>>MAIN<<<<<<=====================================
  
 def main():
     pygame.init()  #inicializar las variables de pygame y sus clases  
     screen = pygame.display.set_mode((WIDTH, HEIGHT)) # tamaño de pantalla 
     pygame.display.set_caption("JuegoMario_YOSHI")#nombre en la ventana
- 
+    reloj= pygame.time.Clock()#reloj para detener el while
    
-    fondo = imagen("imagenes/fondo.png")#llamar imagen fondo      
+    #fondo = imagen("imagenes/fondo.png")#llamar imagen fondo      
     mario = imagen("imagenes/sprites_mario.png",True)#llamarimagen del mario, el true indica que el mario tiene transparencia
     mario_inv=pygame.transform.flip(mario,True,False);# se crea un mario a la inversa ya que transforma la imagen con valores boleanos para invertir en el true en el eje x
-    reloj= pygame.time.Clock()#reloj para detener el while
-    
+    img=pygame.image.load("imagenes/Mario Bros tileset.png")
+    cargar_mapa("Nivel1")
+    hoja=arreglo_Tiles(img)
+
+       
     global saltar_Cur  
     caer=False
     caer_Cur=False
@@ -119,9 +181,18 @@ def main():
         spriteMARIO()
         teclado()
         time= reloj.tick(60)
-        fondo = pygame.transform.scale(fondo, (1000, 400))#transformacion del tamaño de la imagen.
-        screen.blit(fondo, (0, 0))# carga la imagen en el filo superior izquierdo de la imagen.
-        
+       #fondo = pygame.transform.scale(fondo, (1000, 400))#transformacion del tamaño de la imagen.
+        #screen.blit(fondo, (0, 0))# carga la imagen en el filo superior izquierdo de la imagen.
+        for i in range(HeightMap):
+            for j in range(WidthMap):
+                minum=MatrizMapa[i][j]
+                tileImg=hoja[minum-1]
+                tileImg=pygame.transform.scale(tileImg,(tWidth*2,tHeight*2))
+                screen.blit(tileImg,(j*tWidth*2,i*tHeight*2+100))
+        pygame.display.update()
+
+
+
         global PposX,PposY,saltar
         if dire==True and saltar==False:
         	#pinta el matio en direccion a derecha
